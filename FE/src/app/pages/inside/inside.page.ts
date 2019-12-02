123b2331242cc4e74c74a2fa6351f4e0f825f78c
abuzubair <abuzubair.info@gmail.com>
2019-12-02 22:48:07 +0700

    Initial Commit

---
+++ FE/src/app/pages/inside/inside.page.ts
@@ -0,0 +1,47 @@
+import { AuthService } from './../../services/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { ToastController } from '@ionic/angular';
+ 
+@Component({
+  selector: 'app-inside',
+  templateUrl: './inside.page.html',
+  styleUrls: ['./inside.page.scss'],
+})
+export class InsidePage implements OnInit {
+ 
+  data = '';
+  mytoken:any;
+ 
+  constructor(private authService: AuthService, private storage: Storage, private toastController: ToastController) { }
+ 
+  ngOnInit() {
+    this.storage.get('access_token').then(token => {
+      if (token) {
+        this.mytoken = token;
+      }
+    });
+  }
+ 
+  loadSpecialInfo() {
+    this.authService.getSpecialData().subscribe(res => {
+      this.data = res['msg'];
+    });
+  }
+ 
+  logout() {
+    this.authService.logout();
+  }
+ 
+  clearToken() {
+    // ONLY FOR TESTING!
+    this.storage.remove('access_token');
+ 
+    let toast = this.toastController.create({
+      message: 'JWT removed',
+      duration: 3000
+    });
+    toast.then(toast => toast.present());
+  }
+ 
+}
---
+++ api/src/config/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    jwtSecret: 'auth-login-test',
+    db: 'mongodb://127.0.0.1:27017/greatdayhr'
+};
